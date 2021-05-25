36749592d6f98c6cac63dd1694c0d5afdf556226
kevinreber <kevinreber1@gmail.com>
2021-05-24 19:24:33 -0700

    fixed jsx warnings

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Timer = () => {
 	return (
-		<div class="timer-container">
-			<svg viewBox="0 0 75 75" class="progress">
+		<div className="timer-container">
+			<svg viewBox="0 0 75 75" className="progress">
 				<path
-					class="progress-bg"
+					className="progress-bg"
 					d="M18 2.0845
             a 15.9155 15.9155 0 0 1 0 31.831
             a 15.9155 15.9155 0 0 1 0 -31.831"
 				/>
 				<path
-					class="progress-bar"
-					stroke-dasharray="100, 100"
+					className="progress-bar"
+					strokeDasharray="100, 100"
 					d="M18 2.0845
             a 15.9155 15.9155 0 0 1 0 31.831
             a 15.9155 15.9155 0 0 1 0 -31.831"
